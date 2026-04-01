282639dc6bbf7fac53913ee7b3f446f80106eeae
abebraha <abe@icebreakerbd.com>
2026-04-01 05:16:15 -0400

    Update calls.tsx

--- app/(crm)/calls.tsx
+++ app/(crm)/calls.tsx
@@ -10,10 +10,10 @@ import {
   TouchableOpacity,
   Pressable,
 } from 'react-native';
-import { Colors } from 'A/constants/Colors';
-import { FilterBar } from 'A/components/FilterBar';
-import { CallCard } from 'A/components/CallCard';
+import { Colors } from '@/constants/Colors';
+import { FilterBar } from '@/components/FilterBar';
+import { CallCard } from '@/components/CallCard';
 import { LoadingSpinner } from '@/components/LoadingSpinner';
-import { EmptyState } from 'A/components/EmptyState';
+import { EmptyState } from '@/components/EmptyState';
 import { useApi } from '@/hooks/useApi';
 import { fetchCalls, fetchCallAnalysis, Call } from '@/services/api';
